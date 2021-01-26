3088c3a20d2b1bed5b490154c5247d701000bb99
Hrocyber <martirosyanhrayr@gmail.com>
2021-01-26 18:34:50 +0500

    task 8_4

--- task8_4.py
+++
@@ -1,5 +0,0 @@
-# task8_4
-# Create a collection of flowers with their color, get the flowers which color is red, print result
-flowers = {'rose': 'red', 'mexak': 'white', 'kala': 'purple', 'kakach': 'red'}
-red_flowers = list(filter(lambda x: flowers[x] == 'red', flowers))
-print(red_flowers)
